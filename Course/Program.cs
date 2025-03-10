ff87815d56836d3ba20f38e7299d63a51b384739
Marcelo Severo <git@github.com:marceloses/APICatalogo.git>
2025-03-10 19:33:21 -0300

    Funções

--- Course/Program.cs
+++ Course/Program.cs
@@ -13,25 +13,38 @@ namespace Course
         static void Main(string[] args)
         {
             Console.WriteLine("Digite três números");// Este bloco chama o método WriteLine com o argumento "Dig..."
-            
+
             int n1 = int.Parse(Console.ReadLine());
             int n2 = int.Parse(Console.ReadLine());
             int n3 = int.Parse(Console.ReadLine());
-            //
-            if (n1 > n2 && n1 > n3) {
-                Console.WriteLine("Maior = " + n1);
-            }
 
-            else if (n2 > n3)
-            {
-                Console.WriteLine("Maior é o = " + n2);
-            }
+            double resultado = Maior(n1, n2, n3);
 
-            else {
-                Console.WriteLine("Maior é o = " + n3);
-                 }
-
+            Console.WriteLine("Maior = " + resultado);
 
         }
+        static int Maior(int a, int b, int c)
+        {
+            int m;
+
+            if (a > b && a > c)
+            {
+                m = a;
+            }
+            else if (b > c)
+            {
+                m = b;
+            }
+            else
+            {
+                m = c;
+            }
+
+            return m;
+        }
+
+
+
     }
 }
+
