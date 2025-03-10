3e889b1db67ffb0bc266d9363de2a97383c285c8
Marcelo Severo <git@github.com:marceloses/APICatalogo.git>
2025-03-10 19:07:35 -0300

    2 Atualização

--- Course/Program.cs
+++ Course/Program.cs
@@ -17,7 +17,7 @@ namespace Course
             int n1 = int.Parse(Console.ReadLine());
             int n2 = int.Parse(Console.ReadLine());
             int n3 = int.Parse(Console.ReadLine());
-
+            //
             if (n1 > n2 && n1 > n3) {
                 Console.WriteLine("Maior = " + n1);
             }
